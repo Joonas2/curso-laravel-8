481396513c5c32c84416f64d340a2396f3d413a3
Jonas Salles <99370453+Joonas2@users.noreply.github.com>
2022-08-04 23:14:49 -0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,19 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::post('/post', [PostController::class, 'store'])->name('posts.store');
+Route::middleware(['auth'])->group(function(){
 
-Route::get('/posts/{id}', [PostController::class, 'show'])->name('post.show');
-Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
-Route::get( '/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
-Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
-Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index')->middleware(['auth']);
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/post', [PostController::class, 'store'])->name('posts.store');
+
+    Route::get('/posts/{id}', [PostController::class, 'show'])->name('post.show');
+    Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::get( '/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
+    Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
+
+});
 
 Route::get('/dashboard', function () {     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
